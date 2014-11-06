4e791b948b12ce4b3db5fc8ca57f4fadb8eef0c9
Jj <jj@scaldingspoon.com>
2014-11-06 17:16:43 -0600

    added iterator functionality

--- src/main/java/net/unicon/shibboleth/HazelcastStorageService.java
+++ src/main/java/net/unicon/shibboleth/HazelcastStorageService.java
@@ -9,11 +9,13 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@SuppressWarnings("unused")
 public class HazelcastStorageService<K,V> implements StorageService<K,V> {
     private final HazelcastInstance hazelcastInstance;
     private final Map<String, IMap<K,V>> hzMaps = new ConcurrentHashMap<>();
 
 
+    @SuppressWarnings("unused")
     public HazelcastStorageService() {
         this(Hazelcast.newHazelcastInstance());
     }
@@ -24,21 +26,21 @@ public class HazelcastStorageService<K,V> implements StorageService<K,V> {
 
     @Override
     public boolean contains(String partition, K key) {
-        if (key == null) {
-            return false;
-        }
-        return hzMaps.containsKey(partition) && hzMaps.get(partition).containsKey(key);
+        return key != null && hzMaps.containsKey(partition) && hzMaps.get(partition).containsKey(key);
     }
 
     @Override
     public Iterator<String> getPartitions() {
-        return hzMaps.keySet().iterator();
+        return new PartitionIterator();
     }
 
     @Override
     public Iterator<K> getKeys(String partition) {
+        if (partition ==  null) {
+            return null;
+        }
         if (hzMaps.containsKey(partition)) {
-            return hzMaps.get(partition).keySet().iterator();
+            return new PartitionEntryIterator(partition);
         }
         return null;
     }
@@ -63,7 +65,8 @@ public class HazelcastStorageService<K,V> implements StorageService<K,V> {
         }
 
         if (!hzMaps.containsKey(partition)) {
-            hzMaps.put(partition, (IMap<K, V>) this.hazelcastInstance.getMap(partition));
+            IMap<K, V> iMap = this.hazelcastInstance.getMap(partition);
+            hzMaps.put(partition, iMap);
         }
 
         return hzMaps.get(partition).put(key, value);
@@ -80,5 +83,58 @@ public class HazelcastStorageService<K,V> implements StorageService<K,V> {
         }
 
         return null;
+    }
+
+    public class PartitionIterator implements Iterator<String> {
+        private String currentPartition;
+        private final Iterator<String> partitionIterator;
+
+        public PartitionIterator() {
+            this.partitionIterator = hzMaps.keySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.partitionIterator.hasNext();
+        }
+
+        @Override
+        public String next() {
+            this.currentPartition = this.partitionIterator.next();
+            return this.currentPartition;
+        }
+
+        @Override
+        public void remove() {
+            hzMaps.get(this.currentPartition).clear();
+            hzMaps.remove(this.currentPartition);
+        }
+    }
+
+    public class PartitionEntryIterator implements Iterator<K> {
+        private final String partition;
+        private final Iterator<K> keyIterator;
+        private K currentKey;
+
+        public PartitionEntryIterator(String partition) {
+            this.partition = partition;
+            this.keyIterator = hzMaps.get(partition).keySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.keyIterator.hasNext();
+        }
+
+        @Override
+        public K next() {
+            this.currentKey = this.keyIterator.next();
+            return this.currentKey;
+        }
+
+        @Override
+        public void remove() {
+            HazelcastStorageService.this.remove(this.partition, this.currentKey);
+        }
     }
 }
